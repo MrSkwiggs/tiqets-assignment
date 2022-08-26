b22bb95fe0db3b2aeea99c44cf163092a5280f6d
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-08-26 20:25:29 +0200

    💄 Implement layout

--- Tiqets/Tiqets/Scenes/Offerings/OfferingsView.swift
+++ Tiqets/Tiqets/Scenes/Offerings/OfferingsView.swift
@@ -7,43 +7,71 @@
 
 import SwiftUI
 import Kingfisher
+import Core
+import Networking
 
 struct OfferingsView: View {
     @StateObject
     var viewModel: ViewModel
     
     var body: some View {
-        List {
-            Section("Venues") {
-                ForEach(viewModel.venues) { venue in
-                    VStack {
-                        KFImage(venue.imageURL)
-                            .placeholder {
-                                ShimmeringView {
-                                    Rectangle()
-                                }
-                            }
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
+        ScrollView {
+            section(title: "Venues") {
+                LazyVStack(spacing: 16) {
+                    ForEach(viewModel.venues) { venue in
+                        Card(imageURL: venue.imageURL,
+                             title: venue.name,
+                             currency: venue.currency,
+                             price: venue.price)
+                        //                        .frame(height: 100)
                     }
-                    .frame(height: 100)
                 }
             }
             
-            Section("Exhibitions") {
-                ForEach(viewModel.exhibitions) { venue in
-                    VStack {
-                        
+            section(title: "Exhibitions") {
+                LazyVStack(spacing: 16) {
+                    ForEach(viewModel.exhibitions) { exhibition in
+                        Card(imageURL: exhibition.imageURL,
+                             title: exhibition.name,
+                             currency: exhibition.currency,
+                             price: exhibition.price)
                     }
                 }
             }
         }
-        .listStyle(.insetGrouped)
+        .navigationTitle("Offerings")
+    }
+    
+    private func section<Content: View>(title: String,
+                                        @ViewBuilder _ content: () -> Content) -> some View {
+        Section {
+            Group {
+                if viewModel.isLoading {
+                    ForEach(0..<5) { _ in
+                        Card(imageURL: nil, title: "_____", currency: "EUR", price: "3.5")
+                            .loading(isLoading: true)
+                    }
+                } else {
+                    content()
+                }
+            }
+            .padding()
+        } header: {
+            HStack {
+                Text(title.uppercased())
+                    .font(.headline)
+                    .foregroundColor(.gray.opacity(0.5))
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
 struct OfferingsView_Previews: PreviewProvider {
     static var previews: some View {
-        OfferingsView(viewModel: .init())
+        OfferingsView(viewModel: .init(offeringProvider: Mock.OfferingProvider()))
+        
+        OfferingsView(viewModel: .init(offeringProvider: Mock.OfferingProvider(state: .loading)))
     }
 }
