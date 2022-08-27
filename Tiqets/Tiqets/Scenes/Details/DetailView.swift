64abe0e791dbb308a23a761d5b6d6524f198213a
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-08-27 11:07:12 +0200

    💄 Add Favorite Button

--- Tiqets/Tiqets/Scenes/Details/DetailView.swift
+++ Tiqets/Tiqets/Scenes/Details/DetailView.swift
@@ -14,91 +14,114 @@ struct DetailView: View {
     @StateObject
     var viewModel: ViewModel
     
-    let imageURL = URL(string: "https://aws-tiqets-cdn.imgix.net/images/content/9d3735fc2e334bc3a3b68822a2e801b4.jpg?w=315&h=210&dpr=2&q=40&fit=crop")
-    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                VStack {
-                    ZStack(alignment: .leading) {
-                        LinearGradient(colors: [.clear, .clear, .black.opacity(0.9)], startPoint: .top, endPoint: .bottom)
-                        VStack(alignment: .leading) {
-                            Spacer()
-                            Text(viewModel.title)
-                                .font(.title)
-                                .foregroundColor(.white)
-                                .padding()
-                        }
-                    }
-                    .background(
-                        Group {
-                            if let imageURL = viewModel.imageURL {
-                                KFImage(imageURL)
-                                    .placeholder { _ in
-                                        ShimmeringView {}
-                                    }
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                            } else {
-                                ZStack {
-                                    Rectangle()
-                                        .fill(Color.gray.opacity(0.3))
-                                    Image(systemSymbol: .exclamationmarkArrowTriangle2Circlepath)
-                                        .foregroundColor(.secondary)
-                                }
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    VStack {
+                        ZStack(alignment: .leading) {
+                            LinearGradient(colors: [.clear, .clear, .black.opacity(0.9)], startPoint: .top, endPoint: .bottom)
+                            VStack(alignment: .leading) {
+                                Spacer()
+                                Text(viewModel.title)
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .padding()
                             }
                         }
-                    )
-                    .frame(height: 300)
-                    .clipped()
-                }
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    ForEach(viewModel.sections) { section in
-                        switch section.type {
-                        case .price(let currency, let value):
-                            HStack {
-                                Text(currency)
-                                    .font(.footnote)
+                        .background(
+                            Group {
+                                if let imageURL = viewModel.imageURL {
+                                    KFImage(imageURL)
+                                        .placeholder { _ in
+                                            ShimmeringView {}
+                                        }
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                } else {
+                                    ZStack {
+                                        Rectangle()
+                                            .fill(Color.gray.opacity(0.3))
+                                        Image(systemSymbol: .exclamationmarkArrowTriangle2Circlepath)
+                                            .foregroundColor(.secondary)
+                                    }
+                                }
+                            }
+                        )
+                        .frame(height: 300)
+                        .clipped()
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        ForEach(viewModel.sections) { section in
+                            switch section.type {
+                            case .price(let currency, let value):
+                                HStack {
+                                    Text(currency)
+                                        .font(.footnote)
+                                        .foregroundColor(.gray)
+                                    
+                                    Text(value)
+                                        .font(.headline)
+                                        .bold()
+                                }
+                                
+                            case .location(let value):
+                                Text(value)
+                                    .font(.title2)
                                     .foregroundColor(.gray)
                                 
+                            case .rating(let value):
                                 Text(value)
-                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                + Text(" / 5")
                                     .bold()
-                            }
-                            
-                        case .location(let value):
-                            Text(value)
-                                .font(.title2)
-                                .foregroundColor(.gray)
-                            
-                        case .rating(let value):
-                            Text(value)
-                                .foregroundColor(.gray)
-                            + Text(" / 5")
-                                .bold()
-                            
-                        case .dateRange(let startDate, let endDate):
-                            HStack(alignment: .top) {
-                                VStack {
-                                    Text("Starts")
-                                        .foregroundColor(.gray)
-                                    Text(startDate)
-                                        .bold()
-                                }
-                                Spacer()
-                                VStack {
-                                    Text("Ends")
-                                        .foregroundColor(.gray)
-                                Text(endDate)
-                                        .bold()
+                                
+                            case .dateRange(let startDate, let endDate):
+                                HStack(alignment: .top) {
+                                    VStack {
+                                        Text("Starts")
+                                            .foregroundColor(.gray)
+                                        Text(startDate)
+                                            .bold()
+                                    }
+                                    Spacer()
+                                    VStack {
+                                        Text("Ends")
+                                            .foregroundColor(.gray)
+                                        Text(endDate)
+                                            .bold()
+                                    }
                                 }
                             }
                         }
                     }
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
             }
+            Spacer()
+            
+            Button {
+                viewModel.userDidTapFavoriteButton()
+            } label: {
+                Text(
+                    viewModel.isFavorite
+                    ? "Remove from favorites"
+                    : "Add to favorites"
+                )
+                .bold()
+                .foregroundColor(.white)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(
+                            viewModel.isFavorite
+                            ? Color.red
+                            : Color.blue
+                        )
+                )
+            }
+            .loading(isLoading: viewModel.favoriteStateIsLoading)
         }
         .edgesIgnoringSafeArea(.top)
     }
@@ -106,6 +129,7 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(viewModel: .init(Mock.Offerings.Exhibitions.erraticGrowth))
+        DetailView(viewModel: .init(Mock.Offerings.Exhibitions.erraticGrowth,
+                                    favoritesProvider: Mock.FavoritesProvider()))
     }
 }
