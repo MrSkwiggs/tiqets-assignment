65b821ec8cd96ec34b29cb9b49685302182cec53
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-08-27 11:06:05 +0200

    ♻️ Add Favorites Provider to composition

--- Tiqets/Core/Composition.swift
+++ Tiqets/Core/Composition.swift
@@ -11,13 +11,16 @@ import Networking
 public class Composition {
     public let networkPathMonitor: NetworkPathMonitorUseCase
     public let offeringProvider: OfferingProviderUseCase
+    public let favoritesProvider: FavoritesProviderUseCase
     
     public init(tiqetsAPI: TiqetsAPI,
                 networkPathMonitor: NetworkPathMonitorUseCase,
-                offeringProvider: OfferingProviderUseCase) {
+                offeringProvider: OfferingProviderUseCase,
+                favoritesProvider: FavoritesProviderUseCase) {
         TiqetsAPI.configure(using: tiqetsAPI)
         self.networkPathMonitor = networkPathMonitor
         self.offeringProvider = offeringProvider
+        self.favoritesProvider = favoritesProvider
     }
 }
 
@@ -27,20 +30,23 @@ public extension Composition {
     static var main: Composition {
         .init(tiqetsAPI: .main,
               networkPathMonitor: NetworkPathMonitor(),
-              offeringProvider: OfferingProvider())
+              offeringProvider: OfferingProvider(),
+              favoritesProvider: LocalFavoritesProvider()) // use API provider when implemented
     }
     
     /// Uses local values (doesn't depend on API availability) - greater testing & implementation flexibility
     static var debug: Composition {
         .init(tiqetsAPI: .debug,
               networkPathMonitor: NetworkPathMonitor(),
-              offeringProvider: OfferingProvider())
+              offeringProvider: OfferingProvider(),
+              favoritesProvider: LocalFavoritesProvider())
     }
     
     /// Uses mocked data, for a static app state (useful for screenshot & promotional content)
     static var mock: Composition {
         .init(tiqetsAPI: .debug,
               networkPathMonitor: Mock.NetworkPathMonitor(isOnline: true),
-              offeringProvider: Mock.OfferingProvider())
+              offeringProvider: Mock.OfferingProvider(),
+              favoritesProvider: Mock.FavoritesProvider())
     }
 }
