1e75f8b305f2ce697d1bb61514665aa5ca747ed8
MrSkwiggs <6209874+MrSkwiggs@users.noreply.github.com>
2022-08-28 10:27:54 +0200

    ⚡️ Serve DateTimeProvider

--- Tiqets/Core/Composition.swift
+++ Tiqets/Core/Composition.swift
@@ -9,15 +9,18 @@ import Foundation
 import Networking
 
 public class Composition {
+    public let dateTimeProvider: DateTimeProviderUseCase
     public let networkPathMonitor: NetworkPathMonitorUseCase
     public let offeringProvider: OfferingProviderUseCase
     public let favoritesProvider: FavoritesProviderUseCase
     
     public init(tiqetsAPI: TiqetsAPI,
+                dateTimeProvider: DateTimeProviderUseCase,
                 networkPathMonitor: NetworkPathMonitorUseCase,
                 offeringProvider: OfferingProviderUseCase,
                 favoritesProvider: FavoritesProviderUseCase) {
         TiqetsAPI.configure(using: tiqetsAPI)
+        self.dateTimeProvider = dateTimeProvider
         self.networkPathMonitor = networkPathMonitor
         self.offeringProvider = offeringProvider
         self.favoritesProvider = favoritesProvider
@@ -29,6 +32,7 @@ public extension Composition {
     /// Main composition root for production environment
     static var main: Composition {
         .init(tiqetsAPI: .main,
+              dateTimeProvider: DateTimeProvider(),
               networkPathMonitor: NetworkPathMonitor(),
               offeringProvider: OfferingProvider(),
               favoritesProvider: LocalFavoritesProvider()) // use API provider when implemented
@@ -37,6 +41,7 @@ public extension Composition {
     /// Uses local values (doesn't depend on API availability) - greater testing & implementation flexibility
     static var debug: Composition {
         .init(tiqetsAPI: .debug,
+              dateTimeProvider: Mock.DateTimeProvider.firstOfJune2021,
               networkPathMonitor: NetworkPathMonitor(),
               offeringProvider: OfferingProvider(),
               favoritesProvider: LocalFavoritesProvider())
@@ -45,6 +50,7 @@ public extension Composition {
     /// Uses mocked data, for a static app state (useful for screenshot & promotional content)
     static var mock: Composition {
         .init(tiqetsAPI: .debug,
+              dateTimeProvider: Mock.DateTimeProvider(),
               networkPathMonitor: Mock.NetworkPathMonitor(isOnline: true),
               offeringProvider: Mock.OfferingProvider(),
               favoritesProvider: Mock.FavoritesProvider())
